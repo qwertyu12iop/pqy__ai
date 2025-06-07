b363d57ee553fa1c0c2edcfc7e1fa49847da2e43
qwertyu12iop <3355617438@qq.com>
2025-06-07 17:09:36 +0800

    react组件

--- react/todoListComponents/src/components/TodoForm.jsx
+++ react/todoListComponents/src/components/TodoForm.jsx
@@ -1,7 +1,29 @@
-function TodoForm(){
-    return(
-        <form>
+import { useState } from 'react'
+function TodoForm(props) {
+    const onAdd=props.onAdd
+    const [text,setText]=useState('打豆豆')
 
+    const handleSubmit = (e) => {
+        //阻止表单的默认提交行为
+        //由js来控制
+        e.preventDefault();//event api
+        onAdd(text)
+        console.log(e.target.value);
+        //todos? 打报告
+    }
+    const handleChange = (e) => {
+        setText(e.target.value)
+    }
+
+    return (
+        <form action="http://www.baidu.com" onSubmit={handleSubmit}>
+            <input
+                type="text"
+                placeholder="请输入待办事项"
+                value={text}//双向绑定
+                onChange={handleChange}
+            />
+            <button type="submit">添加</button>
         </form>
     )
 }
