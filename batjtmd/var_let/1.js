b166730025c2c5e6ee656495c87eb0f126abd791
qwertyu12iop <3355617438@qq.com>
2025-06-11 19:34:15 +0800

    tts

---
+++ batjtmd/var_let/1.js
@@ -0,0 +1,7 @@
+// "use strict"
+var b=10;
+(function b(){
+    b=20;//不生效的 
+    console.log(b);
+
+})()
---
+++ batjtmd/var_let/2.js
@@ -0,0 +1,9 @@
+//一句代码
+//v8引擎
+//变量提升
+//编译阶段
+//执行阶段
+var a=1;//全局变量 window下
+//console.log(window.a);//window is not defined server
+console.log(global.a);
+
