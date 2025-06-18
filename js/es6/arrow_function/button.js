ae87fb4975b9d50e8d43260fd0017ad77c057f6d
qwertyu12iop <3355617438@qq.com>
2025-06-18 21:34:41 +0800

    箭头函数/css

---
+++ js/es6/arrow_function/button.js
@@ -0,0 +1,14 @@
+//业务流水账代码
+function Button(id){
+    this.element=document.querySelector(`#${id}`);
+    console.log(this.element);
+    
+    this.bindEvent();
+}
+Button.prototype.bindEvent=function(){
+    //this 丢失问题 //this Button
+    this.element.addEventListener('click',this.setBgColor.bind(this))//this指向
+}
+Button.prototype.setBgColor=function(){
+    this.element.style.backgroundColor='#1abc9c'
+}
