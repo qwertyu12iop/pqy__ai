9fd20f05463daab81db390ec8dab27624ce3d346
qwertyu12iop <3355617438@qq.com>
2025-07-03 22:36:54 +0800

    存储，智能前端图片背单词

--- js/storage/cookie-demo/public/script.js
+++ js/storage/cookie-demo/public/script.js
@@ -1,1 +1,40 @@
-console.log('智能前端，智能后端，笑傲秋招')
+// console.log('智能前端，智能后端，笑傲秋招')
+const loginForm=document.getElementById('loginForm')
+loginForm.addEventListener('submit',async(event)=>{
+    event.preventDefault()//阻止表单的默认提交行为
+    //trim()方法用于去除字符串两端的空白字符
+    const username=document.getElementById('username').value.trim()
+    const password=document.getElementById('password').value.trim()
+    //console.log(username,password);
+    try{
+        const response=await fetch('/login',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                username,
+                password
+            })
+        })
+        const data=await response.json()
+        console.log(data);
+        
+    }catch(err){
+        console.log('登录出错了');
+        
+    }
+})
+document.addEventListener('DOMContentLoaded',async ()=>{
+    //登录吗?
+    try{
+        const response=await fetch('/check-login')
+        const data = response.json();
+        console.log(data
+            
+        );
+        
+    }catch(err){
+
+    }
+})
--- react/wordai/shotword/src/components/PictureCard/index.jsx
+++ react/wordai/shotword/src/components/PictureCard/index.jsx
@@ -3,9 +3,17 @@ import React,{
 }from 'react';
 import './style.css'
 
-const PictureCard=()=>{
+const PictureCard=(props)=>{
+    console.log(props,'///');
+    const {
+        uploadImage,
+        word
+
+    }=props;
+    // console.log(uploadImage);
+    
     const [imgPreview, setImgPreview] = useState('https://res.bearbobo.com/resource/upload/W44yyxvl/upload-ih56twxirei.png');
-    const [word,setword]=useState()
+
     const updataImageData=(e)=>{
         //html5的文件上传功能
         //可选链操作符
@@ -19,7 +27,10 @@ const PictureCard=()=>{
             reader.onload=(e)=>{
                 //console.log(reader.result);
                 //响应式业务
-                setImgPreview(reader.result)
+                setImgPreview(reader.result);
+                //如何将图片数据交给父组件
+                uploadImage(reader.result);
+                resolve(reader.result);
 
             }
         })
