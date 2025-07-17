c2dc2916409bad822a8074407accf725df25309a
qwertyu12iop <3355617438@qq.com>
2025-07-17 09:10:50 +0800

    useContext

---
+++ react/context-demo/src/components/Page/index.jsx
@@ -0,0 +1,12 @@
+import Child from '../Child'
+import { useTheme } from '../../hooks/useTheme'
+const Page=()=>{
+    const theme=useTheme()
+    return(
+        <>
+        {theme}
+        <Child />
+        </>
+    )
+}
+export default Page
